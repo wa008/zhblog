1e866856c8021e272e7a5a0f688cde90c17fbcc5
Peng <hzp1024@qq.com>
2024-06-28 16:10:58 +0800

    Update update_rss.py

--- src/update_rss.py
+++ src/update_rss.py
@@ -186,7 +186,7 @@ if __name__ == "__main__":
         limit = 5
         output = './test'
     else:
-        limit = 200
+        limit = 2000
         output = "./../index.md"
     rss_urls = get_urls_from_independent_blogs()
     invite_urls = get_urls_from_valid_blog()
