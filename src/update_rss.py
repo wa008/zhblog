694a0147dc0e67cc195275cca4caf22024eb46fb
wa008 <hzp1024@qq.com>
2024-06-28 14:21:04 +0800

    update

--- src/update_rss.py
+++ src/update_rss.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from rss_parser import RSSParser
-from requests import get
+import requests
 from dateutil import parser
 from datetime import datetime
+from urllib.parse import urlparse
+import re
 import atoma
 
 # rss_url = "http://feeds.feedburner.com/ruanyifeng"
@@ -18,8 +20,9 @@ def if_contain_symbol(keyword):
 
 
 def rss_get_content_from_url(rss_url):
-    response = get(rss_url, timeout = 3)
+    response = requests.get(rss_url, timeout = 1)
     rss = RSSParser.parse(response.text)
+    open('valid_rss.txt', "a").write(rss_url + "\n")
     auther = rss.channel.title.content
     result = []
     index = 0
@@ -33,8 +36,9 @@ def rss_get_content_from_url(rss_url):
     return result
 
 def atom_get_content_from_url(rss_url):
-    response = get(rss_url, timeout = 3)
+    response = requests.get(rss_url, timeout = 1)
     rss = atoma.parse_atom_bytes(response.content)
+    open('valid_rss.txt', "a").write(rss_url + '\n')
     auther = rss.authors[0].name
     result = []
     index = 0
@@ -47,59 +51,124 @@ def atom_get_content_from_url(rss_url):
         if index >= 100: break
     return result
 
-# print (atom_get_content_from_url("https://blog.t9t.io/atom.xml"))
+def get_urls_from_independent_blogs():
+    datas = requests.get('https://raw.githubusercontent.com/timqian/chinese-independent-blogs/master/blogs-original.csv').text.split('\n')[1: ]
+    urls = [x.split(",")[2].strip(' ') for x in datas if len(x.split(',')) >= 3]
+    return urls
 
-datas = open("./blogs-original.csv", 'r').read().split('\n')
-urls = [x.split(",")[2] for x in datas if len(x.split(',')) >= 3]
+def get_urls_from_valid_blog():
+    output_urls = []
+    rss_urls = open('valid_rss.txt', 'r').read().split('\n')
+    rss_urls = list(set(rss_urls))
+    print ('get_urls_from_valid_blog begin', len(rss_urls))
+    index = 0
+    domains = []
+    for url in rss_urls:
+        if len(url) < 2: continue
+        info = urlparse(url.strip(' '))
+        main_page = info.scheme + "://" + info.netloc
+        # print ('main_page: ', main_page)
+        link_suffixs = ['friends', 'link', 'links.html']
+        link_list = []
+        for suffix in link_suffixs:
+            try:
+                link_page = main_page + '/' + suffix
+                # print ('link_page', link_page)
+                res = requests.get(link_page, timeout = 1)
+                link_list = re.findall(r"(?<=href=\").+?(?=\")|(?<=href=\').+?(?=\')", res.text)
+                link_list = [x for x in link_list if main_page not in x]
+                # print ('link_list', link_list)
+                output_urls += link_list
+            except:
+                pass
+        index += 1
+        print ('get_urls_from_valid_blog', index, len(output_urls))
+    output_urls = list(set(output_urls))
+    return output_urls
 
-contents = []
-all_cnt = 0
-skip_cnt = 0
-
-'''
-for url in urls:
-    print (url)
-    all_cnt += 1
-    try:
-        content = atom_get_content_from_url(url)
-    except:
-        content = rss_get_content_from_url(url)
-    contents += content
-'''
-
-for url in urls:
-    print ("url: ", url)
-    all_cnt += 1
-    try:
+def get_blogs_from_rss(rss_urls, limit = 2):
+    contents = []
+    all_cnt = 0
+    skip_cnt = 0
+    print ('get_blogs_from_rss', len(rss_urls))
+    for url in rss_urls:
+        print ("url: ", url)
+        all_cnt += 1
         try:
-            content = atom_get_content_from_url(url)
-            contents += content
+            try:
+                content = atom_get_content_from_url(url)
+                contents += content
+            except:
+                content = rss_get_content_from_url(url)
+                contents += content
         except:
-            content = rss_get_content_from_url(url)
-            contents += content
-    except:
-        skip_cnt += 1
-        print ("except")
-    print ("skip rate: {:} / {:} = {:2f}%".format(skip_cnt, all_cnt, skip_cnt \
-        * 100.0 / all_cnt))
-    if all_cnt >= 2000:
-        break
+            skip_cnt += 1
+            print ("except")
+        print ("skip rate: {:} / {:} = {:2f}%".format(skip_cnt, all_cnt, skip_cnt \
+            * 100.0 / all_cnt))
+        if all_cnt >= limit:
+            break
+    return contents
 
-contents = sorted(contents, key = lambda x: x[0], reverse = True)[: 1000]
+def get_blogs_from_link_urls(link_urls, limit = 2):
+    contents = []
+    all_cnt = 0
+    skip_cnt = 0
+    
+    rss_suffix = ['feed.xml', 'atom.xml', 'feed', 'index.xml', 'rss.xml', 'rss']
+    print ('get_blogs_from_link_url', len(link_urls))
+    for url in link_urls:
+        print ("url: ", url, all_cnt)
+        for suf in rss_suffix:
+            rss = url + '/' + suf
+            all_cnt += 1
+            try:
+                try:
+                    content = atom_get_content_from_url(url)
+                    contents += content
+                except:
+                    content = rss_get_content_from_url(url)
+                    contents += content
+                break
+            except:
+                skip_cnt += 1
+                print ("except")
+        print ("skip rate: {:} / {:} = {:2f}%".format(skip_cnt, all_cnt, skip_cnt \
+            * 100.0 / all_cnt))
+        if all_cnt >= limit:
+            break
+    # print ('link_urls content', contents)
+    return contents
 
-output_file = "./../index.md"
-output = open(output_file, "w")
-output.write("# 中文独立博客\n")
 
-last_day = ""
-output = open(output_file, "a")
-for content in contents:
-    day, title, link, auther = content
-    now_ts = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    if len(title) > 0 and len(link) > 3 and "<![" not in title and if_contain_symbol(title) == False and day < now_ts:
-        today = day.split(" ")[0]
-        if today != last_day:
-            output.write("\n### {}\n".format(today))
-            last_day = today
-        output.write("[{title}]({link})  by  {auther}  at  {day}\n\n".format(title \
-            = title, day = day, link = link, auther = auther))
+def write_output(contents, output_file):
+    contents = sorted(contents, key = lambda x: x[0], reverse = True)[: 1000]
+    output = open(output_file, "w")
+    output.write("# 中文独立博客\n")
+    
+    last_day = ""
+    output = open(output_file, "a")
+    for content in contents:
+        day, title, link, auther = content
+        now_ts = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        if len(title) > 0 and len(link) > 3 and "<![" not in title and if_contain_symbol(title) == False and day < now_ts:
+            today = day.split(" ")[0]
+            if today != last_day:
+                output.write("\n### {}\n".format(today))
+                last_day = today
+            output.write("[{title}]({link})  by  {auther}  at  {day}\n\n".format(title \
+                = title, day = day, link = link, auther = auther))
+
+if __name__ == "__main__":
+    is_test = 1
+    if is_test == 1:
+        limit = 20
+        output = './test'
+    else:
+        limit = 100000
+        output = "./../index.md"
+    rss_urls = get_urls_from_independent_blogs()
+    invite_urls = get_urls_from_valid_blog()
+    # open('valid_rss.txt', "w").write('')
+    contents = get_blogs_from_rss(rss_urls, limit) + get_blogs_from_link_urls(invite_urls, limit)
+    write_output(contents, output)
